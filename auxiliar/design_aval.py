4ddf8ac632f5683e4bb71178f1ba0f246875d5fb
FMoller <fredericomollerper@gmail.com>
2021-07-19 20:32:59 -0300

    more aux tools

--- auxiliar/design_aval.py
+++ auxiliar/design_aval.py
@@ -24,8 +24,26 @@ def collect_des(file):
     a = f.read()
     gen_st = a.find('GENERATION:',find_sat(a))+11
     gen_sp = a.find('\n',gen_st)
+    ver = a.find('NUM TRANSISTORS',gen_st)
     f.close()
-    return int(a[gen_st:gen_sp])
+    if ver != -1:
+        return int(a[gen_st:gen_sp])
+    else:
+        return None
+
+def collect_des_trans(file):
+##    print(file)
+    f = open(file, "r")
+    a = f.read()
+    gen_st = a.find('GENERATION:',find_sat(a))+11
+    ver = a.find('Num transistors:',gen_st)
+    f.close()
+    if ver != -1:
+        ver = ver+16
+        gen_sp = a.find('\n',ver)
+        return int(a[ver:gen_sp])
+    else:
+        return None
 
 def collect_des2(file):
 ##    print(file)
@@ -34,37 +52,115 @@ def collect_des2(file):
     gen_st = a.find('GENERATION:',find_sat(a))+11
     gen_sp = a.find('\t',gen_st)
     f.close()
-    return int(a[gen_st:gen_sp])
+    ver = a.find('NUM TRANSISTORS',gen_st)
+    f.close()
+    if ver != -1:
+        return int(a[gen_st:gen_sp])
+    else:
+        return None
 
 def take_des(directory, problem):
     des_gen = []
     for i in range(1,26):
         try:
-            file = directory+problem+'_'+str(i)
-            des_gen.append(collect_des2(file))
+            try:
+                file = directory+problem+'_'+str(i)
+                des_gen.append(collect_des2(file))
+            except:
+                des_gen.append(collect_des(file))
         except:
+            if problem == 'ccp':
+                problem = 'cc'
             file = directory+problem+'_seed'+str(i)+'.txt'
             des_gen.append(collect_des(file))
+    return des_gen
+
+def take_des_trans(directory, problem):
+    des_gen = []
+    for i in range(1,26):
+        try:
+            file = directory+problem+'_'+str(i)
+            des_gen.append(collect_des_trans(file))
+        except:
+            if problem == 'ccp':
+                problem = 'cc'
+            file = directory+problem+'_seed'+str(i)+'.txt'
+            des_gen.append(collect_des_trans(file))
     return des_gen
 
 prb = "decod"
 a = take_des(dir_ref,prb)
 b = take_des(myn_ref,prb)
+all_data = [b,a]
 
-fig1, ax1 = plt.subplots()
-ax1.set_title("Gerações para chegar em uma solução factível "+prb)
-ax1.violinplot(a,showmeans=True,showmedians=True)
+exit_file = "fig2ff.eps"
+A1 = [['C17','cm42a'],
+     ['cm82a','cm138a']]
+A2 = [['decod','f51m'],
+     ['majority','z4ml']]
+A3 = [['9symml','alu4'],
+     ['cm85a','cm151a']]
+A4 = [['cm162a','cu'],
+     ['x2','x2']]
+A5 = [['ccp','frg1'],
+     ['sct','pm1']]
+A6 = [['t481','tcon'],
+     ['tcon','tcon']]
+
+##A = A6
+##fig, ax = plt.subplots(2,2)
+##plt.rcParams["font.size"] = "20"
+##for i in range(2):
+##    for j in range(2):
+##        ax[i,j].set_title(A[i][j])
+##        all_data = [take_des(myn_ref,A[i][j]),
+##                    take_des(dir_ref,A[i][j])]
+##        for k in all_data:
+##            while None in k:
+##                k.remove(None)
+##        #a=cumulative(a)
+##        #b=cumulative(b)
+##        #c=cumulative(c)
+##        ax[i,j].boxplot(all_data, labels=['n-cgprl','cgp'])
+##        
+##        plt.setp(ax[i,j].get_xticklabels(), fontsize=15)
+##        plt.setp(ax[i,j].get_yticklabels(), fontsize=15)
+##        print(A[i][j])
+##        print(np.min(all_data[0]))
+##        print(np.mean(all_data[0]))
+##        print(np.std(all_data[0]))
+##        print('--------')
+##        print(np.min(all_data[1]))
+##        print(np.mean(all_data[1]))
+##        print(np.std(all_data[1]))
+##        print('########')
+##        
+##
+##fig.subplots_adjust(left=0.1, bottom=0.07, right=0.950,
+##                         top=0.945, wspace=0.12, hspace=0.18)
+##fig.text(0.5, 0.02, 'Algorithm', ha='center')
+##fig.text(0.02, 0.5, 'Generations to achieve a feasible individual', va='center', rotation='vertical')
+##fig1 = plt.gcf()
+##fig1.set_size_inches((13, 11), forward=False)
+##fig.savefig(exit_file,dpi=100, format="eps")        
+##plt.show()
+
+
+
+
+##ax1.set_title("Gerações para chegar em uma solução factível "+prb)
+##ax1.violinplot(all_data,showmeans=True,showmedians=True)
+###ax1.plot([0.7,1.3],[201600,201600])
+###ax1.scatter([1],[np.mean(a)])
+##ax1.grid(True)
+
+#ax1.violinplot(b,showmeans=True,showmedians=True)
 #ax1.plot([0.7,1.3],[201600,201600])
-ax1.scatter([1],[np.mean(a)])
-ax1.grid(True)
+#ax1.scatter([1],[np.mean(b)])
+#ax1.grid(True)
+#fig1.subplots_adjust(left=0.057, bottom=0.067, right=0.974, top=0.95, wspace=None, hspace=None)
 
-ax1.violinplot(b,showmeans=True,showmedians=True)
-#ax1.plot([0.7,1.3],[201600,201600])
-ax1.scatter([1],[np.mean(b)])
-ax1.grid(True)
-fig1.subplots_adjust(left=0.057, bottom=0.067, right=0.974, top=0.95, wspace=None, hspace=None)
 
-plt.show()
 
     
     
---
+++ auxiliar/newmetrics_qual.py
@@ -0,0 +1,107 @@
+import numpy as np
+import pandas as pd
+import design_aval as da
+from math import inf
+pastas = pd.read_csv("pastas_alpha.csv")
+prblm = pd.read_csv("problemas_des.csv")
+print("START")
+dt_struct={"alg":[]}
+dt_dat=dict()
+min_values=dict()
+mean_values=dict()
+min_dat=dict()
+min_datT=dict()
+diff_dat=dict()
+diff_datT=dict()
+mean_dat=dict()
+sr_dat=dict()
+sr_datT=dict()
+#a = leitor.captura(pastas["Pasta"].iloc[i],"ccp")
+#a = leitor.captura(pastas["Pasta"].iloc[i],problema)
+for problema in prblm["Problema"]:
+    if problema == 'cc':
+        problema = 'ccp'
+    for i in range(len(pastas["Pasta"])):
+        if problema not in dt_dat.keys():
+            dt_dat[problema]=dict()
+            mean_values[problema]=[]
+        if problema not in min_values.keys():
+            min_values[problema]=inf
+        try:
+            
+            a = da.take_des_trans(pastas["Pasta"].iloc[i],problema)
+            acp = a.copy()
+            while None in acp:
+                acp.remove(None)
+        except:
+            a=[None]*25
+            acp = []
+        try:
+            if len(acp)>0:
+                amean = np.mean(acp)
+            else:
+                amean = inf
+            amin = np.min(acp)
+        except:
+            amin = inf
+            amean = inf
+        if amin < min_values[problema]:
+            min_values[problema]=amin
+        dt_dat[problema][pastas["algoritmo"].iloc[i]]=a
+        mean_values[problema].append(amean)
+
+#calculo do Best
+for problema in prblm["Problema"]:
+    if problema == 'cc':
+        problema = 'ccp'
+    for i in range(len(pastas["Pasta"])):
+        if problema not in min_dat.keys():
+            min_dat[problema]=dict()
+        if pastas["algoritmo"].iloc[i] not in min_datT.keys():
+            min_datT[pastas["algoritmo"].iloc[i]]=0
+            mean_dat[pastas["algoritmo"].iloc[i]]=0
+        if pastas["algoritmo"].iloc[i] not in diff_dat.keys():   
+            diff_dat[pastas["algoritmo"].iloc[i]]=[]
+            sr_dat[pastas["algoritmo"].iloc[i]]=[]
+        diff = []
+        try:
+            a = da.take_des_trans(pastas["Pasta"].iloc[i],problema)
+            acp = a.copy()
+            while None in acp:
+                acp.remove(None)
+        except:
+            print(pastas["algoritmo"].iloc[i]+" "+problema+" não executado")
+            a=[None]*25
+            acp = []
+        try:
+            if len(acp)>0:
+                amean = np.mean(acp)
+            else:
+                amean = inf
+        except:
+            amean = inf
+        for j in a:
+            if j!=None:
+                if min_values[problema]!=inf:
+                    diff_dat[pastas["algoritmo"].iloc[i]].append((j/min_values[problema])-1)
+                    
+        min_dat[problema][pastas["algoritmo"].iloc[i]]=a.count(min_values[problema])
+        min_datT[pastas["algoritmo"].iloc[i]]+=a.count(min_values[problema])
+        mean_dat[pastas["algoritmo"].iloc[i]]+=np.sum(np.array(mean_values[problema])<amean)
+        sr_dat[pastas["algoritmo"].iloc[i]].append(len(acp))
+for i in diff_dat.keys():
+    try:
+        diff_datT[i]=np.mean(diff_dat[i])
+    except:
+        print(i)
+        
+    sr_datT[i] = np.sum(sr_dat[i])/(len(sr_dat[i])*25)
+            
+        
+dt_struct["alg"] = list(min_datT.keys())
+dt_struct["Best"] = [min_datT[x] for x in min_datT.keys()]
+dt_struct["Mdiff"] = [diff_datT[x] for x in min_datT.keys()]
+dt_struct["Nscore"] = [mean_dat[x] for x in min_datT.keys()]
+dt_struct["SR"] = [sr_datT[x] for x in min_datT.keys()]
+df = pd.DataFrame(data=dt_struct)
+df.to_csv("nmtrics_design80qa.csv")
